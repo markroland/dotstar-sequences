e89db3615704fd8107127fe519eae3347b21bba1
Mark Roland <radiowhir@gmail.com>
2020-08-29 22:42:06 -0700

    Modified clock to fill to the hour hand

--- dotstar_clock.py
+++ dotstar_clock.py
@@ -10,7 +10,7 @@ from datetime import datetime
 import time
 
 # Specify number of pixels
-number_of_leds = 119;
+number_of_leds = 119
 
 # Initialize Dotstar object
 dots = dotstar.DotStar(board.SCK, board.MOSI, number_of_leds, brightness=0.8, auto_write=False)
@@ -25,11 +25,24 @@ def rotate_list(my_list, position):
 # Loop forever
 while True:
 
+    # Get clock hand positions
+    second_index = round((int(datetime.now().strftime('%S')) / 60) * number_of_leds)
+    minute_index = round((int(datetime.now().strftime('%M')) / 60) * number_of_leds)
+    hour_index = round(((int(datetime.now().strftime('%H')) % 12) / 12) * number_of_leds);
+    hour_index += round(
+        ((int(datetime.now().strftime('%M')) / 60) / 12) * number_of_leds
+    )
+    if hour_index > number_of_leds:
+        hour_index = 0
+
     # Initialization. Fill an array of length "number_of_leds" with zeros
     dot_colors = [(0,0,0)] * number_of_leds
 
-    # Debug: Current Time
-    # print(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+    # Solid fill to hours
+    dot_colors[hour_index] = (255, 0, 0)
+    for j in range(number_of_leds):
+        if j < hour_index:
+            dot_colors[j] = (8, 0, 0)
 
     # Set 12 points of clockface
     dot_colors[0] = (0, 64, 0)
@@ -37,21 +50,9 @@ while True:
         index = round((i/12) * number_of_leds)
         dot_colors[index] = (0, 4, 0)
 
-    # Second
-    second_index = round((int(datetime.now().strftime('%S')) / 60) * number_of_leds);
+    # Minute and Second hands
     dot_colors[second_index] = (64, 64, 64)
-
-    # Minute
-    minute_index = round((int(datetime.now().strftime('%M')) / 60) * number_of_leds);
     dot_colors[minute_index] = (0, 0, 255)
-
-    # Hour
-    hour_index = round(((int(datetime.now().strftime('%H')) % 12) / 12) * number_of_leds);
-    # Offset for minutes between the hour
-    hour_index += round(
-        ((int(datetime.now().strftime('%M')) / 60) / 12) * number_of_leds
-    )
-    dot_colors[hour_index] = (255, 0, 0)
 
     # Rotate clockface for the orientation of the viewer
     dot_colors = rotate_list(dot_colors, 75)
