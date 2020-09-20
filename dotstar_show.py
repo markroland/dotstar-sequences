4cf828727a17e0df13004ef7602302a33e38b537
Mark Roland <radiowhir@gmail.com>
2020-09-19 22:13:11 -0700

    Add many sequences

--- dotstar_show.py
+++ dotstar_show.py
@@ -55,6 +55,31 @@ def fade_colors(colors_1, colors_2):
 
     return
 
+def colors_add(colors_1, colors_2):
+
+    colors = [(0,0,0)] * number_of_leds
+
+    # Initialize array
+    for i in range(number_of_leds):
+        # Calculate color components
+        r = min(colors_1[i][0] + colors_2[i][0], 255)
+        g = min(colors_1[i][1] + colors_2[i][1], 255)
+        b = min(colors_1[i][2] + colors_2[i][2], 255)
+
+        # Assign RGB to all LEDs
+        colors[i] = (int(r), int(g), int(b))
+
+    return colors
+
+def render(colors):
+
+    # Assign pattern colors to Dotstar object
+    for i in range(number_of_leds):
+        dots[i] = colors[i]
+
+    # Render LEDs
+    dots.show()
+
 # Parse input
 parser = argparse.ArgumentParser()
 parser.add_argument("-b", "--brightness", type=float, help="Set the LED brigtness, 0.0 to 1.0")
@@ -68,7 +93,7 @@ if args.brightness:
         brightness = args.brightness
 
 # Set time delay between frames
-frame_delay = 0.03;
+frame_delay = 1/60;
 if args.delay:
     if float(args.delay) > 0.0 and float(args.delay) <= 1.0:
         frame_delay = args.delay
@@ -82,13 +107,11 @@ dots = dotstar.DotStar(board.SCK, board.MOSI, number_of_leds, brightness=0.8, au
 # Initialize position
 offset = 0
 
-new_sequence = 1
-
 # Count revolutions
 revolutions = 0;
 steps_per_revolution = int(number_of_leds)
 
-# Load an list of RGB colors for every possible color
+# Load a list of RGB colors for every possible color
 spectrum_colors = get_sinebow_colors(number_of_leds)
 
 # Debugging
@@ -109,32 +132,21 @@ for i in range(number_of_leds):
     time.sleep(0.01)
 time.sleep(2)
 
-# Loop forever
+# Initialize timing
 time_0 = time.time()
-while True:
+time_now = time_0
+
+offset = 0
+revolutions = 0;
+steps_per_revolution = int(number_of_leds)
+
+# Sequence 1
+duration = 3
+while (time_now - time_0) < duration:
 
     time_now = time.time()
 
-    if (time_now - time_0) < 5:
-
-        dot_colors = [(255, 255, 255)] * number_of_leds
-        brightness = breathe(number_of_leds, offset)
-
-    elif (time_now - time_0) < 10:
-
-
-        new_dot_colors = halves(number_of_leds, offset)
-
-        if new_sequence == 1:
-            prev_dot_colors = dot_colors
-            fade_colors(prev_dot_colors, new_dot_colors)
-            new_dot_colors = dot_colors
-            new_sequence = 0
-
-
-    else:
-
-        break;
+    dot_colors = spectrum_straight_across_with_rotation(number_of_leds, revolutions, offset, spectrum_colors)
 
     # Set brightness
     dots.brightness = brightness
@@ -147,15 +159,146 @@ while True:
     dots.show()
 
     # Count total revolutions
-    # if (offset + 1) == steps_per_revolution:
-        # revolutions += 1
+    if (offset + 1) == steps_per_revolution:
+        revolutions += 1
 
     # Increment offset
     offset = (offset + 1) % steps_per_revolution
 
-    # Stop after 1 revolution
-    # if revolutions == 1:
-        # quit()
+    # Delay before iterating through loop
+    # time.sleep(frame_delay)
+
+# Sequence 2
+time_1 = time_now
+new_sequence = 1
+duration = 3
+while (time_now - time_1) < duration:
+
+    time_now = time.time()
+
+    new_dot_colors = halves(number_of_leds, offset)
+
+    if new_sequence == 1:
+        prev_dot_colors = dot_colors
+        fade_colors(prev_dot_colors, new_dot_colors)
+        new_dot_colors = dot_colors
+        new_sequence = 0
+
+    # Spin pattern
+    new_dot_colors = rotate_list(new_dot_colors, offset)
+
+    # Create one dot pattern
+    one_dot_colors = one_dot(number_of_leds, offset)
+
+    # Add patterns
+    new_dot_colors = colors_add(new_dot_colors, one_dot_colors)
+
+    # Set brightness
+    dots.brightness = brightness
+
+    # Assign pattern colors to Dotstar object
+    for i in range(number_of_leds):
+        dots[i] = new_dot_colors[i]
+
+    # Render LEDs
+    dots.show()
+
+    # Increment offset
+    offset = (offset + 1) % steps_per_revolution
+
+    # Delay before iterating through loop
+    time.sleep(frame_delay)
+
+# Sequence 3
+time_1 = time_now
+new_sequence = 1
+duration = 5
+while (time_now - time_1) < duration:
+
+    time_now = time.time()
+
+    new_dot_colors = stripes_spin(number_of_leds, offset, 8)
+
+    render(new_dot_colors)
+
+    # Increment offset
+    offset = (offset + 1) % steps_per_revolution
+
+    # Delay before iterating through loop
+    time.sleep(frame_delay)
+
+# Sequence 4
+time_1 = time_now
+new_sequence = 1
+duration = 5
+while (time_now - time_1) < duration:
+
+    time_now = time.time()
+
+    new_dot_colors = spectrum_fade(number_of_leds, offset, spectrum_colors)
+
+    render(new_dot_colors)
+
+    # Increment offset
+    offset = (offset + 1) % steps_per_revolution
+
+    # Delay before iterating through loop
+    time.sleep(frame_delay)
+
+# Sequence 5
+time_1 = time_now
+new_sequence = 1
+duration = 5
+while (time_now - time_1) < duration:
+
+    time_now = time.time()
+
+    new_dot_colors = spectrum_slide(number_of_leds, offset, spectrum_colors)
+
+    render(new_dot_colors)
+
+    # Increment offset
+    offset = (offset + 1) % steps_per_revolution
+
+    # Delay before iterating through loop
+    time.sleep(frame_delay)
+
+# Sequence 6
+time_1 = time_now
+new_sequence = 1
+duration = 5
+while (time_now - time_1) < duration:
+
+    time_now = time.time()
+
+    new_dot_colors = spectrum_straight_across(number_of_leds, offset, spectrum_colors)
+
+    render(new_dot_colors)
+
+    # Increment offset
+    offset = (offset + 1) % steps_per_revolution
+
+    # Delay before iterating through loop
+    time.sleep(frame_delay)
+
+# Sequence 7
+time_1 = time_now
+new_sequence = 1
+duration = 5
+while (time_now - time_1) < duration:
+
+    time_now = time.time()
+
+    new_dot_colors = spectrum_straight_across_with_rotation(number_of_leds, revolutions, offset, spectrum_colors)
+
+    render(new_dot_colors)
+
+    # Count total revolutions
+    if (offset + 1) == steps_per_revolution:
+        revolutions += 1
+
+    # Increment offset
+    offset = (offset + 1) % steps_per_revolution
 
     # Delay before iterating through loop
     time.sleep(frame_delay)
