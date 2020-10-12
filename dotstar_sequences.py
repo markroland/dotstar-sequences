a8e5688d9d007c5bb6ca55f0aedde98bf12aa07d
Mark Roland <radiowhir@gmail.com>
2020-10-11 21:09:41 -0700

    Added a “sweep” sequence

--- dotstar_sequences.py
+++ dotstar_sequences.py
@@ -8,6 +8,7 @@ import argparse
 import board
 import adafruit_dotstar as dotstar
 import time
+import random
 from sequences import *
 
 # Parse input
@@ -27,7 +28,8 @@ parser.add_argument("pattern", type=str, choices=[
     "spectrum_slide",
     "spectrum_straight_across",
     "spectrum_straight_across_with_rotation",
-    "stripes_spin"
+    "stripes_spin",
+    "sweep"
     ],
     help="Specify the pattern to display"
 )
@@ -120,6 +122,10 @@ elif args.pattern == "spectrum":
     dots.show()
     quit();
 
+# Initialize timing
+time_0 = time.time()
+time_now = time_0
+
 # Loop forever
 while True:
 
@@ -206,6 +212,58 @@ while True:
         elif args.pattern == "stripes_spin":
 
             dot_colors = stripes_spin((200, 0, 0), (0,0,255), number_of_leds, offset)
+
+        elif args.pattern == "sweep":
+
+            # # Fill all to black (off)
+            # dot_colors = [(0, 0, 0)] * number_of_leds
+
+            # # Turn on selected LED pixel
+            # dot_colors[offset] = (255,255,255)
+
+            # # Turn on LED opposite of that one
+            # opposite = (offset + int(number_of_leds/2)) % number_of_leds
+            # dot_colors[opposite] = (255,255,255)
+
+            # ----
+
+            # Set random color on first iteration only
+            try: random_hue
+            except NameError: random_hue = random.random()
+
+            # Define Color 1
+            # color_1 = (0,64,64)
+            color_1_hsv = colorsys.hsv_to_rgb(random_hue, 1.0, 1.0)
+            color_1 = (int(color_1_hsv[0] * 255), int(color_1_hsv[1] * 255), int(color_1_hsv[2] * 255))
+
+            # Define Color 2 as an offset from Color 1 around the Hue value of the HSV Color Wheel
+            # color_2 = (0,0,64)
+            color_2_hsv = colorsys.hsv_to_rgb((random_hue + (1/2)) % 1, 1.0, 1.0)
+            color_2 = (int(color_2_hsv[0] * 255), int(color_2_hsv[1] * 255), int(color_2_hsv[2] * 255))
+
+            # Set all
+            dot_colors = [color_1] * number_of_leds
+
+            time_now = time.time()
+
+            position = math.sin((2*math.pi) * ((time_now - time_0) / 6))
+
+            # # 0 to 59
+            i_position = math.floor(position * (number_of_leds / 4) + (number_of_leds / 4))
+
+            # Top Half
+            # dot_colors[i_position] = color_2
+            for i in range(i_position):
+                dot_colors[i] = color_2
+
+            # Bottom Half
+            # dot_colors[(number_of_leds-1) - i_position] = color_2
+            for i in range((number_of_leds-1) - i_position, number_of_leds-1, 1):
+                dot_colors[i] = color_2
+
+            # Optional: Change orientation for table
+            degrees_rotation = -45
+            dot_colors = rotate_list(dot_colors, math.floor((degrees_rotation/360) * number_of_leds))
 
     else:
         dot_colors = spectrum_straight_across(number_of_leds, offset, spectrum_colors)
