a455ac5d93f961efefd99c6fc33d00dc23db58d5
Mark Roland <radiowhir@gmail.com>
2020-10-16 23:24:26 -0700

    Added Cuttlefish to dotstar_run

--- dotstar_run.py
+++ dotstar_run.py
@@ -37,6 +37,17 @@ def colors_add(colors_1, colors_2):
 
     return colors
 
+def darken(color, darken):
+
+    h = colorsys.rgb_to_hsv(color[0], color[1], color[2])[0]
+    s = colorsys.rgb_to_hsv(color[0], color[1], color[2])[1]
+    v = colorsys.rgb_to_hsv(color[0], color[1], color[2])[2] * darken
+
+    # new_color = colorsys.hsv_to_rgb(h,s,v)
+    new_color = colorsys.hsv_to_rgb(h,s,v)
+
+    return (int(new_color[0]), int(new_color[1]), int(new_color[2]))
+
 # Set Brightness
 brightness = 0.5
 
@@ -287,7 +298,15 @@ while True:
 
         elif day_of_week == 5:
 
-            # Solid
+            # Cuttlefish
+
+            spectrum_colors = get_sinebow_colors(number_of_leds)
+
+            spectrum_offset = 0
+            brightness_offset = 0
+            waves = 12
+
+            dot_colors = [(0, 0, 0)] * number_of_leds
 
             # Loop
             while time_now < time_end:
@@ -295,13 +314,34 @@ while True:
                 # Update time
                 time_now = time.time()
 
-                dot_colors = [(255, 0, 0)] * number_of_leds
+                # Set colors based on spectrum offset
+                dot_colors = [spectrum_colors[spectrum_offset]] * number_of_leds
+                spectrum_offset = (spectrum_offset + 1) % len(spectrum_colors)
 
-                # Render the colors
+                # Modulate HSV lightness value
+
+                # This needs to be a function of Pi so there are no discontinuous jumps
+                brightness_offset = (math.pi / 12) * spectrum_offset
+
+                # Method 1: pulses moving in circle
+                # for i in range(len(dot_colors)):
+                #     lightness_coefficient = 0.6 + (0.4 * math.cos((i/(number_of_leds/waves)) * 2 * math.pi + brightness_offset))
+                #     dot_colors[i] = darken(dot_colors[i], lightness_coefficient)
+
+                # Method 2: Two halves of pulses moving in opposite directions
+                for i in range(number_of_leds):
+                    if i < number_of_leds/2:
+                        lightness_coefficient = 0.6 + (0.4 * math.cos((i/(number_of_leds/waves)) * 2 * math.pi + brightness_offset))
+                        dot_colors[i] = darken(dot_colors[i], lightness_coefficient)
+                    else:
+                        lightness_coefficient = 0.6 + (0.4 * math.cos((i/(number_of_leds/waves)) * 2 * math.pi - brightness_offset))
+                        dot_colors[i] = darken(dot_colors[i], lightness_coefficient)
+
+                # Render to LED strip
                 render(dot_colors)
 
                 # Delay before iterating through loop
-                time.sleep(60)
+                time.sleep(frame_delay)
 
         elif day_of_week == 6:
 
