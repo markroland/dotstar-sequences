55b48203cc67fb01ed7cd73a5b6afafa131b7973
Mark Roland <radiowhir@gmail.com>
2020-10-30 08:55:29 -0700

    Refactored Cuttlefish sequence to use a module

--- cuttlefish.py
+++ cuttlefish.py
@@ -6,75 +6,34 @@
 import board
 import adafruit_dotstar as dotstar
 import time
-import colorsys
-from sequences import *
-
-def render(colors):
-
-    # Assign pattern colors to Dotstar object
-    for i in range(number_of_leds):
-        dots[i] = colors[i]
-
-    # Render LEDs
-    dots.show()
-
-def darken(color, darken):
-
-    h = colorsys.rgb_to_hsv(color[0], color[1], color[2])[0]
-    s = colorsys.rgb_to_hsv(color[0], color[1], color[2])[1]
-    v = colorsys.rgb_to_hsv(color[0], color[1], color[2])[2] * darken
-
-    # new_color = colorsys.hsv_to_rgb(h,s,v)
-    new_color = colorsys.hsv_to_rgb(h,s,v)
-
-    return (int(new_color[0]), int(new_color[1]), int(new_color[2]))
+from sequence.cuttlefish import Cuttlefish
 
 # Set Brightness
 brightness = 0.5
 
 # Set time delay between frames
-frame_delay = 1/60;
+FRAME_DELAY = 1/60;
 
 # Specify number of pixels
-number_of_leds = 119;
+NUMBER_OF_LEDS = 119;
 
 # Option 1: Using a DotStar Digital LED Strip with LEDs connected to hardware SPI
-dots = dotstar.DotStar(board.SCK, board.MOSI, number_of_leds, brightness=brightness, auto_write=False)
+dots = dotstar.DotStar(board.SCK, board.MOSI, NUMBER_OF_LEDS, brightness=brightness, auto_write=False)
 
-# Load a list of RGB colors for every possible color
-spectrum_colors = get_sinebow_colors(number_of_leds)
+# Initialize sequence
+Sequence = Cuttlefish(NUMBER_OF_LEDS)
+dot_colors = Sequence.setup()
 
-spectrum_offset = 0
-brightness_offset = 0
-
+# Loop Forever
 while True:
 
-    # Set colors based on spectrum offset
-    dot_colors = [spectrum_colors[spectrum_offset]] * number_of_leds
-    spectrum_offset = (spectrum_offset + 1) % len(spectrum_colors)
+    # Update sequence
+    dot_colors = Sequence.update()
 
-    # Modulate HSV lightness value
-
-    # This needs to be a function of Pi so there are no discontinuous jumps
-    brightness_offset = (math.pi / 12) * spectrum_offset
-    waves = 12
-
-    # Method 1: pulses moving in circle
-    # for i in range(len(dot_colors)):
-    #     lightness_coefficient = 0.6 + (0.4 * math.cos((i/(number_of_leds/waves)) * 2 * math.pi + brightness_offset))
-    #     dot_colors[i] = darken(dot_colors[i], lightness_coefficient)
-
-    # Method 2: Two halves of pulses moving in opposite directions
-    for i in range(number_of_leds):
-        if i < number_of_leds/2:
-            lightness_coefficient = 0.6 + (0.4 * math.cos((i/(number_of_leds/waves)) * 2 * math.pi + brightness_offset))
-            dot_colors[i] = darken(dot_colors[i], lightness_coefficient)
-        else:
-            lightness_coefficient = 0.6 + (0.4 * math.cos((i/(number_of_leds/waves)) * 2 * math.pi - brightness_offset))
-            dot_colors[i] = darken(dot_colors[i], lightness_coefficient)
-
-    # Render to LED strip
-    render(dot_colors)
+    # Assign pattern colors to Dotstar object and Render
+    for i in range(len(colors)):
+        dots[i] = colors[i]
+    dots.show()
 
     # Delay before iterating through loop
-    time.sleep(frame_delay)
+    time.sleep(FRAME_DELAY)
---
+++ sequence/cuttlefish.py
@@ -0,0 +1,56 @@
+import math
+import colorsys
+import time
+from sequences import get_sinebow_colors
+
+def darken(color, darken):
+
+    h = colorsys.rgb_to_hsv(color[0], color[1], color[2])[0]
+    s = colorsys.rgb_to_hsv(color[0], color[1], color[2])[1]
+    v = colorsys.rgb_to_hsv(color[0], color[1], color[2])[2] * darken
+
+    # new_color = colorsys.hsv_to_rgb(h,s,v)
+    new_color = colorsys.hsv_to_rgb(h,s,v)
+
+    return (int(new_color[0]), int(new_color[1]), int(new_color[2]))
+
+class Cuttlefish:
+
+    def __init__(self, number_of_leds):
+        self.number_of_leds = number_of_leds
+        self.spectrum_colors = get_sinebow_colors(self.number_of_leds)
+        self.spectrum_offset = 0
+        self.brightness_offset = 0
+        self.time_0 = 0
+        self.time = 0
+
+    def setup(self):
+        self.time = time.time() - self.time_0
+        return
+
+    def update(self):
+
+        leds = [self.spectrum_colors[self.spectrum_offset]] * self.number_of_leds
+        self.spectrum_offset = (self.spectrum_offset + 1) % len(self.spectrum_colors)
+
+        # Modulate HSV lightness value
+
+        # This needs to be a function of Pi so there are no discontinuous jumps
+        self.brightness_offset = (math.pi / 12) * self.spectrum_offset
+        waves = 12
+
+        # Method 1: pulses moving in circle
+        # for i in range(len(leds)):
+        #     lightness_coefficient = 0.6 + (0.4 * math.cos((i/(self.number_of_leds/waves)) * 2 * math.pi + self.brightness_offset))
+        #     leds[i] = darken(leds[i], lightness_coefficient)
+
+        # Method 2: Two halves of pulses moving in opposite directions
+        for i in range(self.number_of_leds):
+            if i < self.number_of_leds/2:
+                lightness_coefficient = 0.6 + (0.4 * math.cos((i/(self.number_of_leds/waves)) * 2 * math.pi + self.brightness_offset))
+                leds[i] = darken(leds[i], lightness_coefficient)
+            else:
+                lightness_coefficient = 0.6 + (0.4 * math.cos((i/(self.number_of_leds/waves)) * 2 * math.pi - self.brightness_offset))
+                leds[i] = darken(leds[i], lightness_coefficient)
+
+        return leds
