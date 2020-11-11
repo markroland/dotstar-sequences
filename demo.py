3bd56510d453f484d4a1c6270a6c7a8e6a2ec90b
Mark Roland <radiowhir@gmail.com>
2020-11-10 22:21:30 -0800

    Dynamically import and load Sequence
    This works, but the timing resets during the hand-off from next sequence to current sequence, resulting in a glitch

--- demo.py
+++ demo.py
@@ -6,25 +6,9 @@ import adafruit_dotstar as dotstar
 import time
 from pathlib import Path
 import json
+import importlib
 
-# TODO: Dynamically import
-
-# from pattern.rainbow import *
-from sequence.acceleration import *
-from sequence.breathe import *
-# from sequence.clock import *
-# from sequence.crossing import *
-# from sequence.cuttlefish import *
-# from sequence.points import *
-# from sequence.fade import *
-# from sequence.fire import *
-# from sequence.random import *
-# from sequence.sparkle import *
-# from sequence.spectrum import *
-# from sequence.stripes import *
-# from sequence.textFileDemo import *
-# from sequence.wipe import *
-
+# Add two Lists of colors
 def colors_add(colors_1, colors_2):
 
     colors = [(0,0,0)] * len(colors_1)
@@ -48,6 +32,7 @@ def fade(led_strip, percent):
         faded_colors[i] = (int(led_strip[i][0] * percent), int(led_strip[i][1] * percent), int(led_strip[i][2] * percent))
     return faded_colors
 
+# Fade one List of colors out and another List of colors in over a period of time
 def crossfade(colors_out, colors_in, period, time):
     colors_out_fade = fade(colors_out, 1 - time/period)
     colors_in_fade = fade(colors_in, time/period)
@@ -56,7 +41,7 @@ def crossfade(colors_out, colors_in, period, time):
 # Set defaults prior to input
 NUMBER_OF_LEDS = 119;
 brightness = 0.5
-frame_delay = 1/60;
+FRAME_DELAY = 1/30;
 CROSSFADE_SECONDS = 3
 
 # Read in show configuration from a config file
@@ -68,28 +53,29 @@ with open(str(show_filepath)) as show_json:
 # Using a DotStar Digital LED Strip with LEDs connected to hardware SPI
 dots = dotstar.DotStar(board.SCK, board.MOSI, NUMBER_OF_LEDS, brightness=brightness, auto_write=False)
 
+# Initialize sequences
+Sequence = None
+NextSequence = None
+
 # Loop through all Sequences in the Show
 for i in range(len(show_config)):
 
     # Load Sequence module
-    # TODO: Make this dynamic
-    # https://stackoverflow.com/questions/4821104/dynamic-instantiation-from-string-name-of-a-class-in-dynamically-imported-module
-    if (i == 0):
-        Sequence = Acceleration(NUMBER_OF_LEDS)
-    elif (i == 1):
-        Sequence = Breathe(NUMBER_OF_LEDS)
-
-    # Expand parameters as function arguments
-    Sequence.setup(*show_config[i]['parameters'])
+    if NextSequence is not None:
+        Sequence = NextSequence
+    else:
+        my_module = importlib.import_module(show_config[i]['module'])
+        class_ = getattr(my_module, show_config[i]['name'])
+        Sequence = class_(NUMBER_OF_LEDS)
+        Sequence.setup(*show_config[i]['parameters'])
 
     # For all but the last sequence in the playlist,
     # set up next sequence so the current and next sequences can cross-fade
-    # TODO: Just like above this should dynamically set the next sequence
     if i < len(show_config) - 1:
-        NextSequence = Breathe(NUMBER_OF_LEDS)
+        next_module = importlib.import_module(show_config[i+1]['module'])
+        next_class_ = getattr(next_module, show_config[i+1]['name'])
+        NextSequence = next_class_(NUMBER_OF_LEDS)
         NextSequence.setup(*show_config[i+1]['parameters'])
-    else:
-        NextSequence = None
 
     # Initialize timer
     time_start = time.time()
@@ -115,7 +101,7 @@ for i in range(len(show_config)):
         dots.show()
 
         # Delay before iterating through loop
-        time.sleep(frame_delay)
+        time.sleep(FRAME_DELAY)
 
         # Update timer
         time_now = time.time()
@@ -126,5 +112,5 @@ fade_off_frames = 20
 for i in range(fade_off_frames):
     dots.brightness = brightness - (brightness_start * (i/fade_off_frames))
     dots.show()
-    time.sleep(frame_delay)
+    time.sleep(FRAME_DELAY)
 dots.deinit()
