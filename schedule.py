32d7253fa66d7008be894b230d32e875759becd1
Mark Roland <radiowhir@gmail.com>
2020-11-13 22:36:03 -0800

    Updated Schedule script to read from JSON config

--- schedule.py
+++ schedule.py
@@ -5,50 +5,50 @@
 # Import required libraries
 import board
 import adafruit_dotstar as dotstar
+from datetime import datetime
 import time
 import math
-import random
-from datetime import datetime
-from sequences import *
-from sequence.crossing import Crossing
-from sequence.sparkle import Sparkle
-from sequence.spectrum import *
-from sequence.wipe import *
-
-# Write a List of colors to the Dotstar object and then Show them
-def render(colors):
-
-    # Assign pattern colors to Dotstar object
-    for i in range(number_of_leds):
-        dots[i] = colors[i]
-
-    # Render LEDs
-    dots.show()
+from pathlib import Path
+import json
+import importlib
+import atexit
 
 def fade_brightness(start, end, duration):
-    brightness_start = brightness
-    fade_on_frames = round(duration * (1/frame_delay))
-    for i in range(fade_on_frames):
-        dots.brightness = brightness * (i/fade_on_frames)
+    frames = round(duration * (1/frame_delay))
+    for i in range(frames):
+        if (start > end):
+            dots.brightness = end + ((start - end) * (1-i/frames))
+        else:
+            dots.brightness = start + ((start - end) * (i/frames))
         dots.show()
         time.sleep(frame_delay)
-    dots.brightness = brightness
+
+# Shutdown function to turn lights off if script exits
+def shutdown():
+    fade_brightness(brightness, 0, 1)
+    dots.deinit()
+
+# Register shutdown function
+atexit.register(shutdown)
 
 # Set Brightness
 brightness = 0.5
 
 # Set time delay between frames (Refresh rate)
-frame_delay = 1/60;
+frame_delay = 1/30;
 
 # Specify number of pixels
-number_of_leds = 119;
+NUMBER_OF_LEDS = 119;
 
 # Set start and end hours of the day (i.e. 7am - 10pm)
-start_hour = 6
-end_hour = 22
+START_HOUR = 6
+END_HOUR = 22
 
-# Load color sequence
-spectrum_colors = spectrum("sinebow")
+# Read in show configuration from a config file
+show_filepath = Path(__file__).parent / "shows/weekly.json"
+with open(str(show_filepath)) as show_json:
+    show_config = json.load(show_json)
+    show_json.close();
 
 # Loop forever
 while True:
@@ -59,171 +59,41 @@ while True:
     day_of_week = datetime.now().weekday()
 
     # Define running time
-    time_start = datetime.timestamp(datetime(today.year, today.month, today.day, start_hour, 0, 0))
-    time_end = datetime.timestamp(datetime(today.year, today.month, today.day, end_hour, 0, 0))
+    time_start = datetime.timestamp(datetime(today.year, today.month, today.day, START_HOUR, 0, 0))
+    time_end = datetime.timestamp(datetime(today.year, today.month, today.day, END_HOUR, 0, 0))
 
     # Run this pattern on Friday (day 4 of week)
     if time_now > time_start and time_now < time_end:
 
         # Instantiate a DotStar LED Strip
-        dots = dotstar.DotStar(board.SCK, board.MOSI, number_of_leds, brightness=brightness, auto_write=False)
+        dots = dotstar.DotStar(board.SCK, board.MOSI, NUMBER_OF_LEDS, brightness=brightness, auto_write=False)
 
-        if day_of_week == 0:
+        # Load Sequence
+        index = day_of_week
+        my_module = importlib.import_module(show_config[index]['module'])
+        class_ = getattr(my_module, show_config[index]['name'])
+        Sequence = class_(NUMBER_OF_LEDS)
+        Sequence.setup(*show_config[index]['parameters'])
 
-            # Two colors back and forth
+        # Loop continuously during the time range
+        while time_now < time_end:
 
-            Sequence = Wipe(number_of_leds)
-            hue_1 = random.random()
-            hue_2 = hue_1 + 0.5 % 1
-            Sequence.setup(6, hue_1, hue_2, -45)
+            # Update sequence
+            dot_colors = Sequence.update()
 
-            while time_now < time_end:
+            # Assign pattern colors to Dotstar object
+            for i in range(len(dot_colors)):
+                dots[i] = dot_colors[i]
 
-                dot_colors = Sequence.update()
+            # Render LEDs
+            dots.show()
 
-                # Render to LED strip
-                render(dot_colors)
+            # Delay before iterating through loop
+            time.sleep(frame_delay)
 
-                # Delay before iterating through loop
-                time.sleep(frame_delay)
+            # Update time
+            time_now = time.time()
 
-                # Update time
-                time_now = time.time()
-
-            # Fade Off
-            fade_brightness(brightness, 0, 0.33)
-
-        elif day_of_week == 1:
-
-            # Transition through full spectrum during full run time
-
-            # Fade On
-            dot_colors = [spectrum_colors[0]] * number_of_leds
-            fade_brightness(0, brightness, 0.33)
-
-            Sequence = Spectrum(number_of_leds)
-            Sequence.setup("sinebow", "fade", time_end - time_start)
-
-            while time_now < time_end:
-
-                time_now = time.time()
-
-                dot_colors = Sequence.update()
-
-                # Render to LED strip
-                render(dot_colors)
-
-                # Delay before iterating through loop
-                time.sleep(frame_delay)
-
-            # Fade Off
-            fade_brightness(brightness, 0, 0.33)
-
-        elif day_of_week == 2:
-
-            Sequence = Spectrum(NUMBER_OF_LEDS)
-            Sequence.setup("sinebow", "wipe", 3)
-
-            # Loop
-            while time_now < time_end:
-
-                # Set colors
-                dot_colors = Sequence.update()
-
-                # Render the colors
-                render(dot_colors)
-
-                # Delay before iterating through loop
-                time.sleep(frame_delay)
-
-                # Update time
-                time_now = time.time()
-
-        elif day_of_week == 3:
-
-            Sequence = Spectrum(NUMBER_OF_LEDS)
-            Sequence.setup("sinebow", "slide", 3)
-
-            # Loop
-            while time_now < time_end:
-
-                # Set colors
-                dot_colors = Sequence.update()
-
-                # Render the colors
-                render(dot_colors)
-
-                # Delay before iterating through loop
-                time.sleep(frame_delay)
-
-                # Update time
-                time_now = time.time()
-
-        elif day_of_week == 4:
-
-            # Crossing bands
-
-            Sequence = Crossing(number_of_leds)
-            hue_1 = random.random()
-            # random_hue_2 = random.random()
-            hue_2 = hue_1 + 0.5 % 1
-            length_1 = int(number_of_leds * 0.33)
-            length_2 = int(number_of_leds * 0.2)
-            Sequence.setup(hue_1, hue_2, length_1, length_2)
-
-            while time_now < time_end:
-
-                time_now = time.time()
-
-                dot_colors = Sequence.update()
-
-                # Render to LED strip
-                render(dot_colors)
-
-                # Delay before iterating through loop
-                time.sleep(frame_delay)
-
-        elif day_of_week == 5:
-
-            # Cuttlefish
-
-            # Create and setup a Loop module
-            Sequence = Cuttlefish(number_of_leds)
-            dot_colors = Sequence.setup()
-
-            while time_now < time_end:
-
-                dot_colors = Sequence.update()
-
-                # Render to LED strip
-                render(dot_colors)
-
-                # Delay before iterating through loop
-                time.sleep(frame_delay)
-
-                time_now = time.time()
-
-        elif day_of_week == 6:
-
-            # Create and setup a Loop module
-            sp = Sparkle(number_of_leds)
-            dot_colors = sp.setup()
-
-            # Initialize timing
-            time_0 = time.time()
-            time_now = time_0
-
-            while time_now < time_end:
-
-                time_now = time.time()
-
-                dot_colors = sp.update()
-
-                # Render to LED strip
-                render(dot_colors)
-
-                # Delay before iterating through loop
-                time.sleep(frame_delay)
-
-        # Disable LEDs at end of active time
+        # Fade Off
+        fade_brightness(brightness, 0, 1)
         dots.deinit()
